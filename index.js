093a578a47b1faca7fed0ccfbef5bf4c59a09117
trungkiet2005 <huynhtrungkiet09032005@gmail.com>
2023-01-31 22:52:11 +0700

    up code

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Socket } = require('dgram')
+const express = require('express')
+const app=express()
+
+const http=require('http')
+const { emit } = require('process')
+const server=http.createServer(app)
+const {Server} = require('socket.io')
+
+const io = new Server(server)
+
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname+'/index.html')
+})
+io.on('connection',(socket) =>
+{
+    console.log('user connected')
+
+    socket.on('on-chat', data=>
+    {
+        console.log({data});
+        io.emit('user-chat',data)
+    })
+})
+server.listen(3000,() =>{
+    console.log('listening on port 3000')
+})
